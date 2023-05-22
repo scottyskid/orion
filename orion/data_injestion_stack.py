ed72b700afc85580de223d6d147dc99c7aa0fdce
Scotty Skidmore <scotty.skidmore@gmail.com>
2023-05-22 11:50:52 +1000

    add step function to run data injestion

--- orion/data_injestion_stack.py
+++ orion/data_injestion_stack.py
@@ -1,15 +1,15 @@
 """Stack for injesting data
 """
-from os import path
-
 from constructs import Construct
-from aws_cdk import Stack
-from aws_cdk import aws_apigateway as apigateway
-from aws_cdk import aws_ecr as ecr
-from aws_cdk import aws_ecr_assets as ecr_assets
-from aws_cdk import aws_ecs as ecs
-from aws_cdk.aws_lambda_python_alpha import PythonFunction
-from aws_cdk import aws_s3 as s3
+from aws_cdk import (
+    Stack,
+    aws_ecr_assets as ecr_assets,
+    aws_ecs as ecs,
+    aws_lambda_python_alpha as lambda_python,
+    aws_s3 as s3,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as sfn_tasks,
+)
 
 
 class DataInjestionStack(Stack):
@@ -19,35 +19,26 @@ class DataInjestionStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, config, vpc, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # lambda_dir = config.root_dir / 'lambdas'
-        
-        # # Build python lambda function 
-        # function = aws_lambda_python.PythonFunction(self, 'DataInjestionFunction',
-        #     entry=lambda_dir / 'data_injestion_function',
-
-        # )
-
         # S3 data landing bucket
         s3_bucket = s3.Bucket(self, 'LandingBucket',
             removal_policy=config.removal_policy)
 
-        # container_repo = ecr.Repository(self, 'ContainerRepo',
-        #                                 auto_delete_images=True,
-        #                                 removal_policy=config.removal_policy,
-        #                                 )
+        start_state = sfn.Pass(self, 'StartState')
         
         container = ecr_assets.DockerImageAsset(self, 'Container',
                                                 directory=str(config.root_dir / 'containers' / 'pokeapi_data_upload'))
         
-        cluster = ecs.Cluster(self, 'Cluster', vpc=vpc)
-        
+        cluster = ecs.Cluster(self, 'Cluster',
+                              vpc=vpc)
+
         task_definition = ecs.FargateTaskDefinition(self, 'TaskDefinition')
-        task_definition.add_container('TaskDefinitionContainer', image=ecs.ContainerImage.from_docker_image_asset(container),
-                                      )
+        task_definition.add_container('TaskDefinitionContainer', 
+                                      image=ecs.ContainerImage.from_docker_image_asset(container))
         
+        run_task = sfn_tasks.EcsRunTask(self, 'RunTask',
+                                        cluster=cluster,
+                                        launch_target=sfn_tasks.EcsFargateLaunchTarget(platform_version=ecs.FargatePlatformVersion.LATEST),
+                                        task_definition=task_definition)
         
-        
-
-
-
-
+        start_state.next(run_task)
+        state_machine = sfn.StateMachine(self, 'StateMachine', definition=start_state)
