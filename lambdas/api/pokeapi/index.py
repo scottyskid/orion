1f36b3bd4f3cc6b7588bddec213e186d42d9220e
Scotty Skidmore <scotty.skidmore@gmail.com>
2023-06-06 11:47:46 +1000

    add error handling to lambda

--- lambdas/api/pokeapi/index.py
+++ lambdas/api/pokeapi/index.py
@@ -1,8 +1,18 @@
+""" Lambda function that gets pokeapi data
+"""
+
 import json
 import os
 
 from aws_lambda_powertools import Logger, Tracer
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.event_handler import (
+    APIGatewayRestResolver,
+    Response,
+    content_types,
+)
+from aws_lambda_powertools.event_handler.exceptions import (
+    InternalServerError,
+)
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
@@ -14,47 +24,76 @@ tracer = Tracer()
 logger = Logger()
 app = APIGatewayRestResolver()
 
+
+@app.exception_handler(ValueError)
+def handle_invalid_limit_qs(ex: ValueError):  # receives exception raised
+    metadata: dict = {
+        "path": app.current_event.path,
+        "query_strings": app.current_event.query_string_parameters,
+    }
+    logger.error(f"Malformed request: {ex}", extra=metadata)
+
+    return Response(
+        status_code=400,
+        content_type=content_types.TEXT_PLAIN,
+        body="Invalid request parameters.",
+    )
+
+
+@app.exception_handler(botocore.exceptions.ClientError)
+def handle_botocore_client_error(
+    error: botocore.exceptions.ClientError,
+):  # receives exception raised
+    if error.response["Error"]["Code"] == "NoSuchBucket":
+        logger.exception(
+            f"The bucket doesnt exist. Check the env var is accurate. If local check you have imported environment variables"
+        )
+        return Response(
+            status_code=500,
+            content_type=content_types.TEXT_PLAIN,
+            body="Internal server error, Bucket does not exist",
+        )
+    elif error.response["Error"]["Code"] == "NoSuchKey":
+        logger.exception(f"The key doesnt exist")
+        return Response(
+            status_code=404,
+            content_type=content_types.TEXT_PLAIN,
+            body="Not found",
+        )
+    else:
+        logger.exception(f"Unhandeled Botocore excpetion")
+        raise InternalServerError("Internal server error")
+
+
 @app.get("/api/.+")
 @tracer.capture_method
 def get_file():
-    path = app.current_event.path.rstrip('/')
+    path: str = app.current_event.path.rstrip("/")
 
-    bucket = os.environ['DATA_BUCKET']
-    key = f"{os.environ['DATA_KEY_BASE']}{path}/index.json"
+    bucket: str = os.environ["DATA_BUCKET"]
+    key: str = f"{os.environ['DATA_KEY_BASE']}{path}/index.json"
 
-    logger.info('getting s3 file', bucket=bucket, key=key)
+    logger.info("getting s3 file", bucket=bucket, key=key)
 
-    s3 = boto3.resource('s3') # pylint: disable=invalid-name
+    s3 = boto3.resource("s3")  # pylint: disable=invalid-name
     s3_object = s3.Object(bucket_name=bucket, key=key)
-    try:
-        response = s3_object.get()
-        #TODO Raise 404 on missing file
-        # response.raise_for_status()
-        body = response['Body'].read()
-    except botocore.exceptions.ClientError as error:
-        if error.response['Error']['Code'] == 'NoSuchBucket':
-            logger.exception(f'The bucket {bucket} doesnt exist. Check the param is accurate. If local check you have imported environment variables')
-            raise error
-        else:
-            raise error
+
+    response = s3_object.get()
+    body = response["Body"].read()
 
     # Must return a JSON object
     return json.loads(body)
 
 
-@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
+@logger.inject_lambda_context(
+    correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True
+)
 @tracer.capture_lambda_handler
 def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
-    
     app_resolve = app.resolve(event, context)
 
     return app_resolve
-    
-    #TODO parse in full url at call time ??
 
-    #TODO pagenation and multiple calls - DynamoDB
+    # TODO pagenation and multiple calls - DynamoDB
 
-    #TODO allow searching for name i can use the index in the upper level index.json to do this - DynamoDB
-
-    #TODO implement https://docs.aws.amazon.com/apigateway/latest/developerguide/integrating-api-with-aws-services-s3.html
-    
+    # TODO allow searching for name, i can use the index in the upper level index.json to do this - DynamoDB
